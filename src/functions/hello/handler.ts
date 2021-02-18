8c9d134676a65b1c3da356063b9ba42ff708c44a
Matus Hruz <matus.hruz@edocu.com>
2021-02-18 12:14:46 +0100

    fix demo

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,36 +1,28 @@
 import "source-map-support/register";
 
-import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
-import { formatJSONResponse } from "@libs/apiGateway";
-import { middyfy } from "@libs/lambda";
+import type { ValidatedEventAPIGatewayProxyEvent } from "../../libs/apiGateway";
+import { formatJSONResponse } from "../../libs/apiGateway";
 
 import schema from "./schema";
+console.log(
+  "------------------------------------------------------------------------------------------"
+);
+console.log(__dirname);
+console.log(
+  "-----------------------------------------------------------------------------------------"
+);
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
-  return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
-  });
+  return event.httpMethod === "POST"
+    ? formatJSONResponse({
+        message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+        event,
+      })
+    : formatJSONResponse({
+        message: `Hello World!, welcome to the exciting Serverless world!`,
+      });
 };
 
-export const main = middyfy(hello);
-
-export const helloConfig = {
-  handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
-  name: "hello",
-  events: [
-    {
-      http: {
-        method: "post",
-        path: "hello",
-        request: {
-          schema: {
-            "application/json": schema,
-          },
-        },
-      },
-    },
-  ],
-};
+export const main = hello;
--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,1 +1,29 @@
-export * from "./handler";
+import schema from "./schema";
+
+import { main } from "./handler";
+
+console.log(main.name);
+
+export const helloConfig = {
+  handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
+  name: "hello",
+  events: [
+    {
+      http: {
+        method: "get",
+        path: "hello",
+      },
+    },
+    {
+      http: {
+        method: "post",
+        path: "hello",
+        request: {
+          schema: {
+            "application/json": schema,
+          },
+        },
+      },
+    },
+  ],
+};
